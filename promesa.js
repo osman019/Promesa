abe4adf32987f198c030749174c97f9ee80ddaa2
osman019 <osmanortizrolon@gmail.com>
2024-09-02 14:23:13 -0500

    merge

--- promesa.js
+++ promesa.js
@@ -1,7 +1,7 @@
 // Función que devuelve una promesa que resuelve después de un tiempo especificado
 function cuentaRegresiva(tiempo) {
-    return new Promise((resolve, reject) => {
-        let segundosRestantes = tiempo / 1000; // Convertir milisegundos a segundos
+    return new Promise((resolve,) => {
+        let segundosRestantes = tiempo / 1000; 
 
         const intervalo = setInterval(() => {
             console.log(segundosRestantes + ' segundos restantes');
@@ -9,18 +9,18 @@ function cuentaRegresiva(tiempo) {
 
             if (segundosRestantes < 1) {
                 clearInterval(intervalo); // Detener el intervalo
-                resolve(); // Resuelve  la promesa cuando la cuenta regresiva termine
+                resolve();
             }
-        }, 1000); // ejecuta cada segundo
+        }, 1000); 
     });
 }
 
-// Función que muestra el saludo
+
 function saludo() {
     console.log('Hola osman! Han pasado 5 segundos.');
 }
 
-// promesa con .then, .catch, y .finally
+
 cuentaRegresiva(5000)
     .then(() => {
         saludo(); // Llamar a la función de saludo cuando la promesa se resuelve
